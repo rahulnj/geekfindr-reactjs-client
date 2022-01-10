a34496c26563061f6470f5b505528f19151d0ffd
Rahul <rahuljayaraman8@gmail.com>
2022-01-10 23:47:29 +0530

    added sticky property to both the right and left side bar

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -8,34 +8,64 @@ import { BiComment } from 'react-icons/bi'
 
 const Post: React.FC = () => {
     return (
-        <div className='post'>
-            <div className="post_wrapper">
-                <div className="post_top">
-                    <div className="post_top_left">
-                        <img src={person1} alt="" />
-                        <span className="post_top_left_username">Rahul</span>
-                        <span className="post_top_left_date">5 mins ago</span>
+        <>
+            <div className='post'>
+                <div className="post_wrapper">
+                    <div className="post_top">
+                        <div className="post_top_left">
+                            <img src={person1} alt="" />
+                            <span className="post_top_left_username">Rahul</span>
+                            <span className="post_top_left_date">5 mins ago</span>
+                        </div>
+                        <div className="post_top_right">
+                            <BsThreeDotsVertical className='post_top_right_threedot' />
+                        </div>
                     </div>
-                    <div className="post_top_right">
-                        <BsThreeDotsVertical className='post_top_right_threedot' />
+                    <div className="post_center">
+                        <span className="post_description">Lorem ipsum dolor sit amet consectetur.</span>
+                        <img src={post1} alt="" />
                     </div>
-                </div>
-                <div className="post_center">
-                    <span className="post_description">Lorem ipsum dolor sit amet consectetur.</span>
-                    <img src={post1} alt="" />
-                </div>
-                <div className="post_bottom">
-                    <div className="post_bottom_left">
-                        <AiOutlineLike className='post_bottom_left_icons' />
-                        <BiComment className='post_bottom_left_icons' />
-                        <span className="post_bottom_left_likecounter">10 people liked this</span>
-                    </div>
-                    <div className="post_bottom_right">
-                        {/* <span className="post_commenttext">10</span> */}
+                    <div className="post_bottom">
+                        <div className="post_bottom_left">
+                            <AiOutlineLike className='post_bottom_left_icons' />
+                            <BiComment className='post_bottom_left_icons' />
+                            <span className="post_bottom_left_likecounter">10 people liked this</span>
+                        </div>
+                        <div className="post_bottom_right">
+                            {/* <span className="post_commenttext">10</span> */}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className='post'>
+                <div className="post_wrapper">
+                    <div className="post_top">
+                        <div className="post_top_left">
+                            <img src={person1} alt="" />
+                            <span className="post_top_left_username">Rahul</span>
+                            <span className="post_top_left_date">5 mins ago</span>
+                        </div>
+                        <div className="post_top_right">
+                            <BsThreeDotsVertical className='post_top_right_threedot' />
+                        </div>
+                    </div>
+                    <div className="post_center">
+                        <span className="post_description">Lorem ipsum dolor sit amet consectetur.</span>
+                        <img src={post1} alt="" />
+                    </div>
+                    <div className="post_bottom">
+                        <div className="post_bottom_left">
+                            <AiOutlineLike className='post_bottom_left_icons' />
+                            <BiComment className='post_bottom_left_icons' />
+                            <span className="post_bottom_left_likecounter">10 people liked this</span>
+                        </div>
+                        <div className="post_bottom_right">
+                            {/* <span className="post_commenttext">10</span> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
