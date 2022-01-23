fedcb89afb72a352b104fbcbe10e1c6a16138e18
Rahul <rahuljayaraman8@gmail.com>
2022-01-23 09:56:50 +0530

    changed the tittle and the pages changed to screens in src

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './_app.scss';
 
-import { AuthScreen, HomeScreen, ProfileScreen } from './pages'
+import { AuthScreen, HomeScreen, ProfileScreen } from './screens'
 import { Header, Sidebar } from './components';
 
 import { Route, Routes, useNavigate } from 'react-router-dom';
