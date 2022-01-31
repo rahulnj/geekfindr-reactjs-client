059a510c1dd6795e33300fa7064a9bb4e724c8ae
Rahul <rahuljayaraman8@gmail.com>
2022-01-31 21:11:07 +0530

    form validation added in the authsection

--- src/components/Signin/Signin.tsx
+++ src/components/Signin/Signin.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
+
 import './_Signin.scss'
+
+import validator from "@brocode/simple-react-form-validation-helper";
 
 import { FaUserAlt } from 'react-icons/fa'
 import { RiLockPasswordFill } from 'react-icons/ri'
@@ -10,20 +13,45 @@ import { useTypedSelector } from '../../hooks/useTypedSelector'
 import { useNavigate } from 'react-router-dom'
 
 const Signin: React.FC = () => {
-    const [Email, setEmail] = useState('')
-    const [Password, setPassword] = useState('')
+    const [Email, setEmail] = useState<string>('')
+    const [Password, setPassword] = useState<string>('')
+
+    const [EmailError, setEmailError] = useState<string>('')
+    const [PasswordError, setPasswordError] = useState<string>('')
 
     const { UserSignin } = useActions();
+
     const { data, error, loading } = useTypedSelector(
         (state) => state.UserSignin
     )
 
-    const SignInformSubmition = (e: React.FormEvent) => {
+    const SignInformSubmition = (e: React.SyntheticEvent) => {
         e.preventDefault();
-        UserSignin({
-            email: Email,
-            password: Password
-        })
+
+        if (Email === '' || Password === '') {
+            alert("error")
+        } else if (!EmailError && !PasswordError) {
+            UserSignin({
+                email: Email,
+                password: Password
+            })
+        }
+    }
+
+    const OnChangeEmailValidator = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setEmail(e.target.value);
+        validator.emailInputChangeHandler(e.target.value, setEmailError)
+    }
+    const OnBlurEmailValidator = (e: React.FocusEvent<HTMLInputElement>): void => {
+        validator.emailInputBlurHandler(e.target.value, setEmailError)
+    }
+
+    const OnChangePasswordValidator = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setPassword(e.target.value);
+        validator.passwordInputChangeHandler(e.target.value, setPasswordError)
+    }
+    const OnBlurPasswordValidator = (e: React.FocusEvent<HTMLInputElement>): void => {
+        validator.passwordInputBlurHandler(e.target.value, setPasswordError)
     }
 
     const navigate = useNavigate();
@@ -37,14 +65,16 @@ const Signin: React.FC = () => {
 
         <form onSubmit={SignInformSubmition} className="signin">
             <h2 className="signin_title">Sign in</h2>
-            <div className="signin_inputfield" >
+            <div className={EmailError ? "signin_inputfield error_inputfield" : "signin_inputfield"} >
                 <FaUserAlt className='signin_inputfield_icons' />
-                <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                <input type="text" placeholder="Email" onChange={OnChangeEmailValidator} onBlur={OnBlurEmailValidator} />
             </div >
-            <div className="signin_inputfield" >
+            <div className='signin_error'>{EmailError}</div>
+            <div className={PasswordError ? "signin_inputfield error_inputfield" : "signin_inputfield"} >
                 <RiLockPasswordFill className='signin_inputfield_icons' />
-                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                <input type="password" placeholder="Password" onChange={OnChangePasswordValidator} onBlur={OnBlurPasswordValidator} />
             </div >
+            <div className='signin_error'>{PasswordError}</div>
             <input type="submit" value="Login" className="signin_btn" />
             <p className="signin_socialtext" > Or Sign in with social platforms</p >
             <div className="signin_socialmedia" >
--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
+
 import './_Signup.scss'
+
+import validator from "@brocode/simple-react-form-validation-helper";
 
 import { FaUserAlt } from 'react-icons/fa'
 import { SiGmail } from 'react-icons/si'
@@ -15,23 +18,55 @@ import { useNavigate } from 'react-router-dom'
 
 const Signup: React.FC = () => {
 
-    const [Username, setUsername] = useState('')
-    const [Email, setEmail] = useState('')
-    const [Password, setPassword] = useState('')
+    const [Username, setUsername] = useState<string>('')
+    const [Email, setEmail] = useState<string>('')
+    const [Password, setPassword] = useState<string>('')
+
+    const [UsernameError, setUsernameError] = useState<string>('')
+    const [EmailError, setEmailError] = useState<string>('')
+    const [PasswordError, setPasswordError] = useState<string>('')
 
     const { UserSignup } = useActions();
     const { user, data, error, loading } = useTypedSelector(
         (state) => state.UserSignup
     );
 
-    const SignUpFormSubmition = (e: React.FormEvent) => {
+    const SignUpFormSubmition = (e: React.SyntheticEvent) => {
         e.preventDefault();
-        UserSignup({
-            username: Username,
-            email: Email,
-            password: Password,
+        if (Username === '' || Email === '' || Password === '') {
+            alert("error")
+        }
+        else if (!UsernameError && !EmailError && !PasswordError) {
+            UserSignup({
+                username: Username,
+                email: Email,
+                password: Password,
+            })
+        }
+    }
 
-        })
+    const OnChangeUsernameValidator = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setUsername(e.target.value);
+        validator.nameInputChangeHandler(e.target.value, setUsernameError)
+    }
+    const OnBlurUsernameValidator = (e: React.FocusEvent<HTMLInputElement>): void => {
+        validator.nameInputBlurHandler(e.target.value, setUsernameError)
+    }
+
+    const OnChangeEmailValidator = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setEmail(e.target.value);
+        validator.emailInputChangeHandler(e.target.value, setEmailError)
+    }
+    const OnBlurEmailValidator = (e: React.FocusEvent<HTMLInputElement>): void => {
+        validator.emailInputBlurHandler(e.target.value, setEmailError)
+    }
+
+    const OnChangePasswordValidator = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setPassword(e.target.value);
+        validator.passwordInputChangeHandler(e.target.value, setPasswordError)
+    }
+    const OnBlurPasswordValidator = (e: React.FocusEvent<HTMLInputElement>): void => {
+        validator.passwordInputBlurHandler(e.target.value, setPasswordError)
     }
 
     const navigate = useNavigate();
@@ -42,26 +77,29 @@ const Signup: React.FC = () => {
     }, [data, user])
 
     return (
-        <form onSubmit={SignUpFormSubmition} >
+        <form onSubmit={SignUpFormSubmition}  >
             <h2 className="signup_title">Sign up</h2>
-            <div className="signup_inputfield">
+            <div className={UsernameError ? "signup_inputfield error_inputfield" : "signup_inputfield"}>
                 <FaUserAlt className='signup_inputfield_icons' />
-                <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" placeholder="Username" onChange={OnChangeUsernameValidator} onBlur={OnBlurUsernameValidator} />
             </div>
-            <div className="signup_inputfield">
+            <div className='signup_error'>{UsernameError}</div>
+            <div className={EmailError ? "signup_inputfield error_inputfield" : "signup_inputfield"}>
                 <SiGmail className='signup_inputfield_icons' />
-                <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                <input type="email" placeholder="Email" onChange={OnChangeEmailValidator} onBlur={OnBlurEmailValidator} />
             </div>
-            <div className="signup_inputfield">
+            <div className='signup_error'>{EmailError}</div>
+            <div className={PasswordError ? "signup_inputfield error_inputfield" : "signup_inputfield"}>
                 <RiLockPasswordFill className='signup_inputfield_icons' />
-                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                <input type="password" placeholder="Password" onChange={OnChangePasswordValidator} onBlur={OnBlurPasswordValidator} />
             </div>
+            <div className='signup_error'>{PasswordError}</div>
             <input type="submit" className="signup_btn" value="Sign up" />
             <p className="signup_socialtext">Or Sign up with social platforms</p>
             <div className="signup_socialmedia">
                 <BsGithub className='signup_icon' />
             </div>
-        </form>
+        </form >
     )
 }
 
