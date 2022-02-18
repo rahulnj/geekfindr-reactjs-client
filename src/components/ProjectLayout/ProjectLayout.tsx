9e44c95e39d13c668a05906cc34ddb44dacac544
Rahul <rahuljayaraman8@gmail.com>
2022-02-18 11:36:13 +0530

    changes in the project layout

--- src/components/ProjectLayout/ProjectLayout.tsx
+++ src/components/ProjectLayout/ProjectLayout.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './_ProjectLayout.scss'
 
 const ProjectLayout = () => {
+    const [descriptionActive, setDescriptionActive] = useState(true)
+    const [teamActive, setTeamActive] = useState(false)
+    const [todoActive, setTodoActive] = useState(false)
+    const [taskActive, setTaskActive] = useState(false)
+
+    const SelectActiveSection = (section: string) => {
+        if (section === 'description') {
+            setDescriptionActive(true)
+            setTeamActive(false)
+            setTodoActive(false)
+            setTaskActive(false)
+        } else if (section === 'team') {
+            setDescriptionActive(false)
+            setTeamActive(true)
+            setTodoActive(false)
+            setTaskActive(false)
+        } else if (section === 'todo') {
+            setDescriptionActive(false)
+            setTeamActive(false)
+            setTodoActive(true)
+            setTaskActive(false)
+        } else if (section === 'tasks') {
+            setDescriptionActive(false)
+            setTeamActive(false)
+            setTodoActive(false)
+            setTaskActive(true)
+        }
+    }
     return (
         <div className='projectlayout'>
             <div className="projectlayout_header">
@@ -11,14 +39,34 @@ const ProjectLayout = () => {
             </div>
             <div className='projectlayout_wrapper'>
                 <div className="projectlayout_titles">
-                    <div className='projectlayout_title1'>
+                    <div className={descriptionActive ? 'projectlayout_title-1 active' : 'projectlayout_title-1'}
+                        onClick={() => SelectActiveSection('description')}
+                    >
                         <span>
                             <h3>Description</h3>
                         </span>
                     </div>
-                    <div className='projectlayout_title'>Team</div>
-                    <div className='projectlayout_title'>Todo</div>
-                    <div className='projectlayout_title'>Tasks</div>
+                    <div className={teamActive ? 'projectlayout_title-2 active' : 'projectlayout_title-2'}
+                        onClick={() => SelectActiveSection('team')}
+                    >
+                        <span>
+                            <h3>Team</h3>
+                        </span>
+                    </div>
+                    <div className={todoActive ? 'projectlayout_title-3 active' : 'projectlayout_title-3'}
+                        onClick={() => SelectActiveSection('todo')}
+                    >
+                        <span>
+                            <h3>Todo</h3>
+                        </span>
+                    </div>
+                    <div className={taskActive ? 'projectlayout_title-4 active' : 'projectlayout_title-4'}
+                        onClick={() => SelectActiveSection('tasks')}
+                    >
+                        <span>
+                            <h3>Tasks</h3>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
