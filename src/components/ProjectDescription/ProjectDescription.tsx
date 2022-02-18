ca742d96d5bb7b661d94fb9ccca1a80bafb5f136
Rahul <rahuljayaraman8@gmail.com>
2022-02-18 12:28:34 +0530

    added the project description page

--- src/components/ProjectDescription/ProjectDescription.tsx
+++ src/components/ProjectDescription/ProjectDescription.tsx
@@ -4,7 +4,19 @@ import './_ProjectDescription.scss'
 
 const ProjectDescription = () => {
     return (
-        <div>ProjectDescription</div>
+        <div className='projectdescription'>
+            <div className="projectdescription_header">
+                <h3>Brocode</h3>
+            </div>
+            <hr />
+            <div className="projectdescription_short">
+                <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tenetur ad necessitatibus quaerat ipsa deleniti error pariatur debitis mollitia, eveniet.</span>
+            </div>
+            <div className="projectdescription_long">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Tempore et consequuntur, optio iusto expedita quia dignissimos quod voluptas officia deleniti. Sunt asperiores eius aliquid ea harum error fugit consequuntur placeat.</p>
+            </div>
+        </div>
     )
 }
 
