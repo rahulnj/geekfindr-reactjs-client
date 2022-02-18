dcf6fbd2cd217a2ae4bef8169d61cf37f74eedbc
Rahul <rahuljayaraman8@gmail.com>
2022-02-18 12:59:22 +0530

    created the project team page

--- src/components/ProjectTeam/ProjectTeam.tsx
+++ src/components/ProjectTeam/ProjectTeam.tsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 import './_ProjectTeam.scss'
 
+import post1 from '../../assets/persons/1.jpeg'
+
 const ProjectTeam = () => {
     return (
-        <div>ProjectTeam</div>
+        <div className='projectteam'>
+            <div className="projectteam_header">
+                <h3>Teammates</h3>
+            </div>
+            <hr />
+            <div className="projectteam_user">
+                <div className="projectteam_user_left">
+                    <img src={post1} alt="" />
+                    <div className="projectteam_user_left_body">
+                        <h5>Rahul</h5>
+                        <p>Frontend developer</p>
+                    </div>
+                </div>
+                <div className="projectteam_user_right">
+                    {/* space for the buttons */}
+                </div>
+            </div>
+        </div>
     )
 }
 
