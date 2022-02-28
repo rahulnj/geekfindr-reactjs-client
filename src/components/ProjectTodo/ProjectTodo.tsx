bed15f82e2f4802754f722d3a4d54fdfb1694f91
Rahul <rahuljayaraman8@gmail.com>
2022-02-28 23:10:00 +0530

    added the project todo

--- src/components/ProjectTodo/ProjectTodo.tsx
+++ src/components/ProjectTodo/ProjectTodo.tsx
@@ -5,7 +5,39 @@ import './_ProjectTodo.scss'
 
 const ProjectTodo = () => {
     return (
-        <div>ProjectTodo</div>
+        <div className='projecttodo'>
+            <div className='projecttodo_header'>
+                <h3>Status</h3>
+            </div>
+            <hr />
+            <div className="projecttodo_wrapper">
+                <div className="projecttodo_wrapper_nostatus">
+                    <div className='projecttodo_header2'>
+                        <h3>No Status</h3>
+                    </div>
+                    <hr />
+
+                </div>
+                <div className="projecttodo_wrapper_nextup">
+                    <div className='projecttodo_header2'>
+                        <h3>Next Up</h3>
+                    </div>
+                    <hr />
+                </div>
+                <div className="projecttodo_wrapper_inprogress">
+                    <div className='projecttodo_header2'>
+                        <h3>In Progress</h3>
+                    </div>
+                    <hr />
+                </div>
+                <div className="projecttodo_wrapper_completed">
+                    <div className='projecttodo_header2'>
+                        <h3>Completed</h3>
+                    </div>
+                    <hr />
+                </div>
+            </div>
+        </div>
     )
 }
 
