f077ca5ca294410fa7e56fe66afd185415960b99
Rahul <rahuljayaraman8@gmail.com>
2022-03-01 10:19:49 +0530

    changes in the project todo

--- src/components/ProjectTodo/ProjectTodo.tsx
+++ src/components/ProjectTodo/ProjectTodo.tsx
@@ -1,13 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './_ProjectTodo.scss'
 
-
+interface Todo {
+    id: number
+    todo: string
+    isNoStatus: Boolean
+    isNextUp: boolean
+    isInProgress: boolean
+    isCompleted: boolean
+}
 const ProjectTodo = () => {
+
+    const [todo, setTodo] = useState('')
+    const [noStatus, setNoStatus] = useState<Todo[]>([])
+    const [nextUp, setNextUp] = useState<Todo[]>([])
+    const [inProgress, setInProgress] = useState<Todo[]>([])
+    const [completed, setCompleted] = useState<Todo[]>([])
+
+
+    const handleAddTodo = () => {
+        if (todo) {
+            setNoStatus([...noStatus, {
+                id: Date.now(), todo, isNoStatus: true,
+                isNextUp: false, isInProgress: false, isCompleted: false
+            }])
+            setTodo('')
+        }
+        const finalTodo = {
+            todo: [
+                {
+                    title: 'nostatus',
+                    tasks: [
+                        ...noStatus,
+                    ]
+                },
+                {
+                    title: 'nextup',
+                    tasks: [
+
+                    ]
+                },
+                {
+                    title: 'inprogress',
+                    tasks: [
+
+                    ]
+                },
+                {
+                    title: 'completed',
+                    tasks: [
+
+                    ]
+                }
+            ]
+        }
+    }
+    console.log(noStatus);
+
+
     return (
         <div className='projecttodo'>
             <div className='projecttodo_header'>
                 <h3>Status</h3>
+                <div className='projecttodo_input'>
+                    {/* <label>Add +</label> */}
+                    <input type="text" placeholder='Add ...' value={todo}
+                        onChange={(e) => setTodo(e.target.value)} />
+                    <button className="projecttodo_buttonadd"
+                        onClick={handleAddTodo}
+                    >Add+</button>
+                </div>
             </div>
             <hr />
             <div className="projecttodo_wrapper">
@@ -16,6 +79,13 @@ const ProjectTodo = () => {
                         <h3>No Status</h3>
                     </div>
                     <hr />
+                    {noStatus?.map((todo: Todo) => (
+                        <div key={todo?.id} className='projecttodo_singletodo'>
+                            {todo?.todo}
+                        </div>
+                    ))
+                    }
+
 
                 </div>
                 <div className="projecttodo_wrapper_nextup">
