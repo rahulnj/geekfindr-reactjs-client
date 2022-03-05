6fbc80bfdf2ef21d27793b7e46d28aa4be791994
Rahul <rahuljayaraman8@gmail.com>
2022-03-05 11:25:29 +0530

    changes in the project tasks style

--- src/components/ProjectTasks/ProjectTasks.tsx
+++ src/components/ProjectTasks/ProjectTasks.tsx
@@ -6,7 +6,32 @@ import './_ProjectTasks.scss'
 const ProjectTasks = () => {
     return (
         <div className='projecttasks'>
-            <div className="projecttasks_wrapper">
+            <div className="projecttasks_header">
+                <h3>Tasks</h3>
+                <div className='projecttasks_input'>
+                    <button className="projecttasks_buttonassign"
+                    >+ Assign Tasks</button>
+                </div>
+            </div>
+            <hr />
+            <div className='projecttasks_singletask'>
+                <div className='projecttasks_singletask_left'>
+                    <h3>
+                        Tittle
+                    </h3>
+                    <p>
+                        description for the project tasks.
+                    </p>
+                </div>
+                <div className='projecttasks_singletask_center'>
+                    <span>
+                        badge
+                    </span>
+                </div>
+                <div className='projecttasks_singletask_right'>
+                    <button className="projecttasks_buttonassign"
+                    >Manage</button>
+                </div>
             </div>
         </div>
     )
