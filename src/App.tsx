48c6c5c6f7a777cd8067edb4005dc5b473215fef
Rahul <rahuljayaraman8@gmail.com>
2022-03-20 15:59:31 +0530

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 
 import { AuthState } from './models';
 
-import { Layout } from './components';
-import { AuthScreen, ChatScreen, EditDetailsScreen, HomeScreen, PageNotFound, ProfileScreen, ProjectScreen } from './screens'
+import { Layout, Spinner } from './components';
 
 import { useTypedSelector } from './hooks/useTypedSelector';
 
+const HomeScreen = lazy(() => import('./screens/HomeScreen/HomeScreen'))
+const AuthScreen = lazy(() => import('./screens/AuthScreen/AuthScreen'))
+const ChatScreen = lazy(() => import('./screens/ChatScreen/ChatScreen'))
+const EditDetailsScreen = lazy(() => import('./screens/EditDetailsScreen/EditDetailsScreen'))
+const ProfileScreen = lazy(() => import('./screens/ProfileScreen/ProfileScreen'))
+const ProjectScreen = lazy(() => import('./screens/ProjectScreen/ProjectScreen'))
+const PageNotFound = lazy(() => import('./screens/PageNotFoundScreen/PageNotFound'))
 
 const App: React.FC = () => {
 
@@ -36,53 +42,63 @@ const App: React.FC = () => {
     <>
       <Routes>
         <Route path='/' element={
-          <Layout>
-            <HomeScreen />
-          </Layout>
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <Layout>
+              <HomeScreen />
+            </Layout>
+          </Suspense>
+
+        } />
         <Route path='/auth' element={
-          <AuthScreen />
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <AuthScreen />
+          </Suspense>
+        } />
         <Route path='/profile/:id' element={
-          <Layout>
-            <ProfileScreen />
-          </Layout>
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <Layout>
+              <ProfileScreen />
+            </Layout>
+          </Suspense>
+        } />
         <Route path='/userprofile/:userId' element={
-          <Layout>
-            <ProfileScreen userProfile />
-          </Layout>
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <Layout>
+              <ProfileScreen userProfile />
+            </Layout>
+          </Suspense>
+        } />
         <Route path='/editprofile/:id' element={
-          <EditDetailsScreen />
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <EditDetailsScreen />
+          </Suspense>
+        } />
         <Route path='/editpost/:postId' element={
-          <Layout>
-            <ProfileScreen />
-          </Layout>
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <Layout>
+              <ProfileScreen />
+            </Layout>
+          </Suspense>
+        } />
         <Route path='/project/:projectId' element={
-          <Layout project>
-            <ProjectScreen />
-          </Layout>
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <Layout project>
+              <ProjectScreen />
+            </Layout>
+          </Suspense>
+        } />
         <Route path='/chat' element={
-          <Layout project>
-            <ChatScreen />
-          </Layout>
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <Layout project>
+              <ChatScreen />
+            </Layout>
+          </Suspense>
+        } />
         <Route path='*' element={
-          <PageNotFound />
-        }>
-        </Route>
+          <Suspense fallback={<Spinner loader={true} />}>
+            <PageNotFound />
+          </Suspense>
+        } />
       </Routes>
     </>
   );
